3a43f94a210e5ad13658059de4709f46409f7f24
Gabriel Amaral <gabrielamaral@Gabriels-MacBook-Air.local>
2022-11-30 08:49:31 -0700

    Walls working completely

--- src/useMovement.js
+++ src/useMovement.js
@@ -1,74 +1,131 @@
 import { useState, useEffect } from 'react';
 
 export default function useMovement( gridArray = null ) {
-    let speed = 16;
+    let speed = 3;
 
     const pixelSize = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--pixel-size'));
     const gridCell = 16;  //Size of a block, just 16 because for some reason it already accounts pixel size
-    const MAX_X_POSITION = (gridCell * 15); //Size of a block, times 15 blocks (accounting for start 0), and -5 to account for charater size
-    const MAX_Y_POSITION = (gridCell * 15);
+    const MAX_X_POSITION = (gridCell * 15)+6; //Size of a block, times 15 blocks (accounting for start 0), and -5 to account for charater size
+    const MAX_Y_POSITION = (gridCell * 15)+3;
     const [facingPosition, setFacingPosition] = useState('');
-    // let [x, setX] = useState(0);
-    // let [y, setY] = useState(gridCell*4);
+
     let [[x, y], setLocation] = useState([0, gridCell*4]);
-    console.log([x,y])
     const [walking, setWalking] = useState('false');
 
+    const mapWidth = 16;
+    const mapLength = 16;
     const cameraLeft = pixelSize * 65;
     const cameraTop = pixelSize * 42;
 
-    const mapWidth = 16
     let moveUp = () => {
         console.log("Up arrow was pressed, move Up");
         setFacingPosition('face-up');
-        setLocation(([prevX, prevY]) => prevY > 0 && movementAllowed(DIRECTION.UP, (prevY/gridCell)*mapWidth+(prevX/gridCell)) ? [prevX, prevY - speed] : [prevX, prevY]);
+        setLocation(([prevX, prevY]) => prevY > 0 && movementAllowed('up', prevX, prevY) ? [prevX, prevY - speed] : [prevX, prevY]);
     };
     const moveRight = () => {
         console.log("Right arrow was pressed, move Right");
         setFacingPosition('face-right');
-        setLocation(([prevX, prevY]) => prevX < MAX_X_POSITION && movementAllowed(DIRECTION.RIGHT, (prevY/gridCell)*mapWidth+(prevX/gridCell)) ? [prevX + speed, prevY] : [prevX, prevY]);
+        setLocation(([prevX, prevY]) => prevX < MAX_X_POSITION && movementAllowed('right', prevX, prevY) ? [prevX + speed, prevY] : [prevX, prevY]);
     }
     const moveDown = () => {
         console.log("Down arrow was pressed, move Down");
         setFacingPosition('face-down');
-        setLocation(([prevX, prevY]) => prevY < MAX_Y_POSITION && movementAllowed(DIRECTION.DOWN, (prevY/gridCell)*mapWidth+(prevX/gridCell)) ? [prevX, prevY + speed] : [prevX, prevY]);
+        setLocation(([prevX, prevY]) => prevY < MAX_Y_POSITION && movementAllowed('down', prevX, prevY) ? [prevX, prevY + speed] : [prevX, prevY]);
     }
     const moveLeft = () => {
         console.log("Left arrow was pressed, move Left");
         setFacingPosition('face-left');
-        setLocation(([prevX, prevY]) => prevX > 0 && movementAllowed(DIRECTION.LEFT, (prevY/gridCell)*mapWidth+(prevX/gridCell)) ? [prevX - speed, prevY] : [prevX, prevY]);
+        setLocation(([prevX, prevY]) => prevX > 0 && movementAllowed('left', prevX, prevY) ? [prevX - speed, prevY] : [prevX, prevY]);
     }
 
-    // gridArray = 16 x 16
-    gridArray = gridArray ? gridArray : [
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0,
-        0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1,
-        0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1,
-        0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1,
-        0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,
-        0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    ]
+    gridArray = creatingArrayforMap(mapLength, mapWidth);
+    makingMapWalls();
 
-    const DIRECTION = {
-        LEFT: -1,
-        RIGHT: +1,
-        UP: -16,
-        DOWN: +16
+    // Map array 256x256
+    function creatingArrayforMap(mapLength, mapWidth) {
+        let array = new Array((gridCell*mapLength));
+        for(let i = 0; i < array.length; i++) {
+            array[i] = new Array((gridCell*mapWidth))
+        }
+        return array;
     }
 
-    const movementAllowed = (direction, coordinate) => {
-        console.log(gridArray[coordinate+direction])
-        return coordinate+direction > 0 && coordinate+direction < 16*16 && gridArray[coordinate+direction] === 0
+    function makingMapWalls() {
+        for(let x = 0; x < gridArray.length; x++){
+            for(let y = 0; y < gridArray[x].length; y++){
+                // Walls for the 2 hills on the top
+                if (x <= 3) {
+                    gridArray[y][x] = 1;
+                }
+                else if (x < gridCell*4-6) {
+                    if (y < (gridCell*7)-2) {
+                        gridArray[y][x] = 1;
+                    }
+                    else if (y > gridCell*8+3) {
+                        gridArray[y][x] = 1;
+                    }
+                }
+                // Walls for middle pool
+                if (x > gridCell*6+4 && x <= (gridCell*11)) {
+                    if (x <= gridCell*7+4) {
+                        if (y >= gridCell*7+6 && y < gridCell*10-6) {
+                            gridArray[y][x] = 1;
+                        }
+                    }
+                    else if (y >= gridCell*5+6 && y < gridCell*10-6) {
+                        gridArray[y][x] = 1;
+                    }
+                }
+                // Walls for left pool
+                if (x > (gridCell*8)+4 && x <= (gridCell*13)) {
+                    if (y > gridCell-8 && y < gridCell*3-8) {
+                        gridArray[y][x] = 1;
+                    }
+                }
+                // Walls for right Bush
+                if (x > gridCell*8+3 && x <= gridCell*14-6) {
+                    if (x <= gridCell*9+3) {
+                        if (y > gridCell*14+3) {
+                            gridArray[y][x] = 1;
+                        }
+                    }
+                    else if (x <= gridCell*10+3) {
+                        if (y > gridCell*12) {
+                            gridArray[y][x] = 1;
+                        }
+                    }
+                    else if (y > gridCell*11+3) {
+                        gridArray[y][x] = 1;
+                    }
+                }
+            }
+        }
+    }
+
+
+    const movementAllowed = (direction, coordinateX, coordinateY) => {
+        let nextPixelCoordinateX
+        let nextPixelCoordinateY
+
+        if (direction === 'right') {
+            nextPixelCoordinateX = coordinateX + speed;}
+        else if (direction === 'left') {
+            nextPixelCoordinateX = coordinateX - speed;}
+        else {
+            nextPixelCoordinateX = coordinateX}
+        if (direction === 'down') {
+            nextPixelCoordinateY = coordinateY + speed;}
+        else if (direction === 'up') {
+            nextPixelCoordinateY = coordinateY - speed;}
+        else {
+            nextPixelCoordinateY = coordinateY}
+
+        console.log(gridArray[coordinateX][coordinateY]);
+        console.log(gridArray[nextPixelCoordinateX][nextPixelCoordinateY]);
+        console.log(`(${coordinateX}, ${coordinateY})`);
+        console.log(`(${nextPixelCoordinateX}, ${nextPixelCoordinateY})`);
+
+        return nextPixelCoordinateX >= 0 && nextPixelCoordinateY >= 0 && nextPixelCoordinateX <= gridCell*mapLength && nextPixelCoordinateY <= gridCell*mapWidth && !gridArray[nextPixelCoordinateX][nextPixelCoordinateY];
     }
 
     const handleKeyDown = (e) => {
